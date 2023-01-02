a266c947bd2bd4bae2ed5eef19df00e487f9ece4
André Carlos <95436218+iwaandore@users.noreply.github.com>
2023-01-02 18:17:32 -0300

    commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function showContact(element) {
+    document.getElementById(element).style.visibility = 'visible';
+}
+
+function redirectLinkedin() {
+    location.href = 'https://www.linkedin.com/in/andr%C3%A9-carlos-822245254/'
+}
+
+function redirectGithub() {
+    location.href = 'https://github.com/iwaandore'
+}
+
+function redirectGmail() {
+    location.href = 'mailto:ac7718288.ac@gmail.com'
+}
+
+function redirectInstagram() {
+    location.href = 'https://www.instagram.com/iwa_andore/'
+}
